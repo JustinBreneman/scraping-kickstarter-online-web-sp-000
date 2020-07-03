554413dc6feee4951da178a64de9e7649c1225f5
Justin Breneman <justin.breneman@yahoo.com>
2020-07-03 00:15:54 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 def create_project_hash
   html = File.read('fixtures/Kickstarter.html')
-  Kickstarter = Nokogiri::HTML(html)
+  kickstarter = Nokogiri::HTML(html)
   binding.pry
 end
 
